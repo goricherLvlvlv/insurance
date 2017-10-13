4c2a18a0483f47bdac524ab2e530d0b6fa901af9
goricher <307363020@qq.com>
2017-10-13 21:13:41 +0800

    1.将'保障内容'冗杂的json格式换成dict格式,再导入数据库 2.将'保障内容'无意义的数据删除了

--- XYZ_insurance.py
+++ XYZ_insurance.py
@@ -69,26 +69,30 @@ class HtmlParser(object):       # 爬虫的解析器类
             res_data['title'] = title_node.get_text(strip=True)
 
             title_info = soup.find_all('div',class_="hc-form-item hc-clearFix")     # 产品特色 承保年龄...
-            # res_data['on_sale'] = title_info[0]
             title_info.pop(0)
             res_data['info'] = title_info
             product_spe=''
             for info in res_data['info']:
                 product_spe+=info.get_text(strip=True)
-            # json_info={'info':info_summary}
-            #collection.insert({'title': res_data['title'], 'url': res_data['url'], 'product_special': product_spe})
-            collection.update({'title':res_data['title']},{'$set':{'title':res_data['title'],'url':res_data['url'],'product_special':product_spe}})        # 添加数据到数据库
-            safeguard_content = soup.find('input', id='dev_benefitesCategoryJson').get('value')      # 获取'保障内容'json数据
+            safeguard_content_temp = soup.find('input', id='dev_benefitesCategoryJson').get('value')      # 获取'保障内容'json数据
             insurance_notice_temp = soup.find_all('div', class_='product-detail__content hc-ckeditor')     # 获取'投保须知'
             insurance_notice_temp.pop(0)
             insurance_notice = ''
             for notice in insurance_notice_temp:
                 insurance_notice += notice.get_text(strip=True)
-            #print(safeguard_content)
-            collection.update({'title':res_data['title']},{'$set':{'safeguard_content':safeguard_content}})
-            collection.update({'title':res_data['title']},{'$set':{'insurance':insurance_notice}})      # 将'保障内容'与'投保须知'加如数据库
+            json_datas=json.loads(safeguard_content_temp)
+            safeguard_content=[]
+
+            for json_data in json_datas:
+                for a_data in json_data['protectPropDTOs']:
+                    print(a_data['optionDTOs'])
+                    safeguard_content+=[{'name':a_data['name'],'explanation':a_data['explanation']}]
+            #collection.insert({'title': res_data['title'], 'url': res_data['url'], 'product_special': product_spe})
+            collection.update({'title': res_data['title']}, {'$set': {'title': res_data['title'], 'url': res_data['url'],'product_special': product_spe}})        # 添加数据到数据库
+            collection.update({'title':res_data['title']},{'$set':{'safeguard_content': safeguard_content,'insurance':insurance_notice}})      # 将'保障内容'与'投保须知'加如数据库
                                                                                                             # '保障内容'中含有大量无用数据,且保障内容的json格式 在数据库中没有良好的体现
             #collection.remove()  # 删除数据库
+
         return res_data
 
 class SpiderMain(object):       # 爬虫类
@@ -107,7 +111,7 @@ class SpiderMain(object):       # 爬虫类
                                                                             # 或者爬取保险页面的内容 分别赋值给new_urls和new_data
                 self.urls.add_new_urls(new_urls)        # 若成功爬取new_urls,将获取的url串加入url管理器的url串中
                 count=count+1       # 计数器加1
-                time.sleep(1)       # 当前网页不能接受过于频繁的访问,延迟+1s 0--0
+                time.sleep(1.5)       # 当前网页不能接受过于频繁的访问,延迟+1s 0--0
 
             except:
                 print('craw failed')
