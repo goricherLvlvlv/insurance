371ff7573e40653332435448fd8fbdf8044f3bf1
wbzhong <1343192175@qq.com>
2017-10-15 19:56:01 +0800

    Add files via upload
    添加上了产品对应url及注释

--- get_company.py
+++ get_company.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 try:
     header={'user-agent':'Mozilla/5.0'}
@@ -11,18 +12,25 @@ try:
         soup=BeautifulSoup(t1.text,"html.parser")
         i=0
         count = 0
-        new_companys=[]
-        name=[]
+        names=[]
+        new_companys = []
+        new_urls = []
         b=soup.find('div',id='dev_prodList')
-        all_c=b.find_all('span',class_='hazardC_pro_con_company')
-        d_all=b.find_all('a',class_='f16 dev_trialSuccess')
-        for d in d_all:
-            name.append(d.text.strip())
+        all_companys=b.find_all('span',class_='hazardC_pro_con_company')
+        all_names=b.find_all('a',class_='f16 dev_trialSuccess')
 
-        for c in all_c:
+        links = soup.find_all('a', class_="hazardC_pro_toSee dev_trialSuccess")  # 获取所有"去看看"按钮的跳转页面的标签
+        for link in links:
+            new_url = link['href']
+            new_urls = urljoin(root_url, new_url)  # 用page_url和href来拼接保险页面的完整的url
+
+        for d in all_names:                        # 获取产品名称
+            names.append(d.text.strip())
+
+        for c in all_companys:                     # 获取产品对应的公司
             new_companys.append(c.text.strip())
             count = count +1
-            print(count, name[i],'  ',new_companys[i])
+            print(count, names[i], '  ', new_companys[i], '  ', new_urls)       # 输出产品名，公司，及url
             i=i+1
         ID = ID+1
 except:
