833558a0511f56a51390f92844ad69ca9ef7acb4
oneone1995 <wanglixian1995@live.com>
2017-10-17 08:12:16 +0800

    fix typos

--- get_company.py
+++ get_company.py
@@ -13,10 +13,10 @@ try:
         i = 0
         count = 0
         names = []
-        new_companys = []
+        new_company = []
         new_urls = []
         b = soup.find('div', id='dev_prodList')
-        all_companys = b.find_all('span', class_='hazardC_pro_con_company')
+        all_company = b.find_all('span', class_='hazardC_pro_con_company')
         all_names = b.find_all('a', class_='f16 dev_trialSuccess')
 
         links = soup.find_all('a', class_="hazardC_pro_toSee dev_trialSuccess")  # 获取所有"去看看"按钮的跳转页面的标签
@@ -27,10 +27,10 @@ try:
         for d in all_names:  # 获取产品名称
             names.append(d.text.strip())
 
-        for c in all_companys:  # 获取产品对应的公司
-            new_companys.append(c.text.strip())
+        for c in all_company:  # 获取产品对应的公司
+            new_company.append(c.text.strip())
             count = count + 1
-            print(count, names[i], '  ', new_companys[i], '  ', new_urls)  # 输出产品名，公司，及url
+            print(count, names[i], '  ', new_company[i], '  ', new_urls)  # 输出产品名，公司，及url
             i = i + 1
         ID = ID + 1
 except:
