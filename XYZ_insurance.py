1ca9a77d5e54b40b0f5f6bdce1808199f81e0775
goricher <307363020@qq.com>
2017-10-18 17:28:31 +0800

    1.新增了一个爬取公司名的.py文件 2.成功将公司名导入数据库

--- XYZ_insurance.py
+++ XYZ_insurance.py
@@ -3,13 +3,12 @@ import json
 import time
 from urllib.parse import urljoin
 from urllib.request import urlopen
-
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import company_craw
 connection = MongoClient('localhost', 27017)  # 数据库连接
 db = connection.insurance
 collection = db.XYZ
-
 def download(url):  # url下载
     if url is None:
         return None
@@ -46,17 +45,20 @@ class UrlManager(object):  # url管理器类
 
 
 class HtmlParser(object):  # 爬虫的解析器类
-    def parse(self, page_url, html_cont):
+    def parse(self, page_url, html_cont,d_url_company):
         if page_url is None or html_cont is None:
             return
         soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')  # 配置网页解析器
         new_urls = self._get_new_urls(page_url, soup)
-        new_data = self._get_new_data(page_url, soup)
+        new_data = self._get_new_data(page_url, soup,d_url_company)
         return new_urls, new_data
 
     def _get_new_urls(self, page_url, soup):
-        if page_url.split('/p')[0] == 'http://www.xyz.cn/mall/jiankangxian':  # 判断是否为主页面,若是则去获取跳转页面
-            # 若为保险页面则无需去获取跳转页面
+        """
+            判断是否为主页面,若是则去获取跳转页面
+            若为保险页面则无需去获取跳转页面
+        """
+        if page_url.split('/p')[0] == 'http://www.xyz.cn/mall/jiankangxian':
             new_urls = set()
             links = soup.find_all('a', class_="hazardC_pro_toSee dev_trialSuccess")  # 获取所有"去看看"按钮的跳转页面的标签
             for link in links:
@@ -65,14 +67,13 @@ class HtmlParser(object):  # 爬虫的解析器类
                 new_urls.add(new_full_url)
             return new_urls
 
-    def _get_new_data(self, page_url, soup):
+    def _get_new_data(self, page_url, soup,d_url_company):
         res_data = {'url': page_url}
         l = page_url.split('/')
         # connection = MongoClient('localhost', 27017)  # 数据库连接
         # db = connection.insurance
         # collection = db.XYZ
         if l[4] != "jiankangxian":  # 判断是否为保险页面,若是则获取下面的数据
-            # 该区域代码还未写完 后续需要添加数据库
             title_node = soup.find('h1', class_="product-intro__title-text")
             res_data['title'] = title_node.get_text(strip=True)
             title_info = soup.find_all('div',class_="hc-form-item hc-clearFix")     # 产品特色 承保年龄...
@@ -95,9 +96,12 @@ class HtmlParser(object):  # 爬虫的解析器类
                     safeguard_content += [{'name':a_data['name'],'explanation':a_data['explanation']}]
             collection.insert({'title': res_data['title'], 'url': res_data['url'], 'product_special': product_spe})
             #collection.update({'title': res_data['title']}, {'$set': {'title': res_data['title'], 'url': res_data['url'], 'product_special': product_spe}})        # 添加数据到数据库
-            collection.update({'title':res_data['title']}, {'$set': {'safeguard_content': safeguard_content, 'insurance': insurance_notice}})      # 将'保障内容'与'投保须知'加如数据库
-                                                                                                                 # '保障内容'中含有大量无用数据,且保障内容的json格式 在数据库中没有良好的体现
-            #collection.remove()  # 删除数据库
+            """
+            将'保障内容'与'投保须知'加如数据库
+            '保障内容'中含有大量无用数据,且保障内容的json格式 在数据库中没有良好的体现
+           """
+            collection.update({'title':res_data['title']}, {'$set': {'company':d_url_company[page_url], 'safeguard_content': safeguard_content, 'insurance': insurance_notice}})
+            #print(d_url_company)
         return res_data
 
 
@@ -106,7 +110,7 @@ class SpiderMain(object):  # 爬虫类
         self.parser = HtmlParser()
         self.urls = UrlManager()
 
-    def craw(self, root_url):
+    def craw(self, root_url,d_url_company):
         count = 1
         self.urls.add_new_url(root_url)  # 将根url添加到url串里
         while self.urls.has_new_url():
@@ -114,7 +118,7 @@ class SpiderMain(object):  # 爬虫类
                 new_url = self.urls.get_new_url()     # url串里弹出url
                 print('craw %d : %s'%(count, new_url))       # 输出对应的保险序号以及网址
                 html_cont = download(new_url)     # 下载网页内容
-                new_urls, new_data = self.parser.parse(new_url,html_cont)      # 把content传入parser中爬取各保险的网址
+                new_urls, new_data = self.parser.parse(new_url,html_cont,d_url_company)      # 把content传入parser中爬取各保险的网址
                                                                             # 或者爬取保险页面的内容 分别赋值给new_urls和new_data
                 self.urls.add_new_urls(new_urls)        # 若成功爬取new_urls,将获取的url串加入url管理器的url串中
                 count = count+1       # 计数器加1
@@ -132,7 +136,8 @@ if __name__ == "__main__":
     # obj_spider.craw(root_url)
     # 多页面爬虫
     collection.remove()
+    d_url_company = company_craw.company_main()
     while ID <= 10:
         root_url = base_url + '/p' + str(ID) + '.html'
-        obj_spider.craw(root_url)
+        obj_spider.craw(root_url,d_url_company)
         ID = ID + 1
